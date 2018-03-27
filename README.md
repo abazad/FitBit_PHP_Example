40161981ed639864c8e5d7ea1da37efc0539d4a0
Srivani Bahatam <srivani.bahatam@valuelabs.com>
2018-03-27 17:13:07 +0530

    FITBIT Api complete example initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FitBit_PHP_Example
 A complete PHP example to retrieve data from FitBit
+
+Update Clinet Id and Secret in Config file.
+
+Make sure you have the same callback in the fitbit app.
+
+
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('CLIENT_ID', 'XXXXXXXXX');
+define('CLIENT_SECRET', 'XXXXXXXXXXXXXXXXXXXXXXX');
+define('REDIRECT_URI', 'http://localhost/FitBit_PHP_Example/callback.php');
+define('HOST', 'https://api.fitbit.com/');
+define('ACCESS_TOKEN_URL', 'https://api.fitbit.com/oauth2/token');
+define('AUTHENTICATE_URL', 'https://www.fitbit.com/oauth2/authorize');
+
+?>
